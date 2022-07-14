387a9ce86024598d7d81146ffd278ea3a07ce27f
Sean Hong <seanhong2000@gmail.com>
2022-07-15 03:17:14 +0900

    feat: Add MySQL to ELK Stack integration

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@db:3306/dev_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@mysql:3306/dev_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
