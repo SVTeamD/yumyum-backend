705de381dd7aa53c471aebb64c0b0b0358693fef
홍성민 <seanhong2000@gmail.com>
2022-07-16 17:41:31 +0900

    style: rename variable pointing to S3 bucket

--- backend/aws/bucket.py
+++ backend/aws/bucket.py
@@ -1,14 +1,14 @@
 from typing import List
 
 import boto3
-from .config import S3_Bucket
+from .config import S3_BUCKET
 from .connection import connect
 
 def post_bucket(image_file: str, key_name: str):
     client = connect()
     client.put_object(
         Body=image_file, 
-        Bucket=S3_Bucket,
+        Bucket=S3_BUCKET,
         Key=key_name,
         ContentType="image.jpeg"
     )
