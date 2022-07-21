19105fd2bced456768d7bb779bfc1be787d792e7
jung <mira20932@gmail.com>
2022-07-21 15:26:52 +0900

    refactoring: 1_schemas.py

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -3,7 +3,7 @@ from typing import List, Union
 from pydantic import BaseModel
 
 
-class MenuBase(BaseModel):
+class MenuBase(BaseModel): # 메뉴 테이블
     id: int
     class Config:
         orm_mode = True
@@ -33,20 +33,67 @@ class MenuDelete(MenuBase):
     is_active: bool
 
 
-class CategoryCreate(BaseModel):
+class MerchantBase(BaseModel): # merchant 테이블
+    id: int
+    class Config:
+        orm_mode = True
+
+class Merchant(MerchantBase):
     ...
+
+class  MerchantCreate():
+    ...
+
+class CategoryBase(BaseModel): # 카테고리 테이블
+    id: int
+    class Config:
+        orm_mode = True
+
+class Category(CategoryBase):
+    name: str
+
+class CategoryCreate(Category): 
+    pass
     
 
 class CategoryRead(CategoryCreate):
-   ...
+   pass
 
-class StoreCreate(BaseModel):
+
+class StoreBase(BaseModel): # 가게 테이블
+    ...
+    class Config:
+        orm_mode = True
+
+
+class Store(StoreBase):
+    ...
+
+
+class StoreCreate(BaseModel): 
     merchant_id: int
     menu_id: int
     category_name: str
     location_id: int
     name: str
     photo_url: str
+
+class StoreRead():
+    ...
+
+class StoreDelete():
+    ...
+
+class LocationBase(BaseModel): # 가게위치 테이블
+    ...
+    class Config:
+        orm_mode = True
+
+class Location(LocationBase):
+
+
+
+
 
 
 
