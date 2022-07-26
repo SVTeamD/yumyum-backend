3602822ae9f7eb3600c6c342032ebda1c4106d4b
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-07-26 15:45:40 +0900

    add: backend api

---
+++ backend/api/endpoints/menus.py
@@ -0,0 +1,53 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from crud import crud
+from schemas import schemas
+from api.dep import get_db
+
+router = APIRouter()
+
+
+# TODO: 에러 처리
+
+# 메뉴 생성
+@router.post("", response_model = schemas.MenuCreate) # menu api
+def create_menu_info(menu: schemas.MenuCreate, db: Session = Depends(get_db)):
+    return crud.create_menu(db, menu = menu)
+
+
+# 메인 메뉴 정보 조회
+@router.get("/main", response_model=List[schemas.Menu])
+def read_main_menu(db: Session = Depends(get_db)):
+    menus = crud.get_main_menu(db)
+    return menus
+
+
+# 메뉴 상세 조회
+@router.get("/{menu_id}")
+def read_menu_by_id(menu_id: int, db: Session = Depends(get_db)):
+    menus = crud.get_menu_by_id(db, menu_id=menu_id)
+    return menus
+
+
+# 메뉴 이름으로 검색 
+@router.get("/name/{menu_name}")
+def read_menu_by_name(menu_name: str, db: Session = Depends(get_db)):
+    menus = crud.get_menu_by_name(db, menu_name=menu_name)
+    return menus
+
+
+# 메인 메뉴 만들어주기
+@router.put("/main/{menu_id}")
+def update_menu_by_id(menu_id: int, db: Session = Depends(get_db)):
+    response = crud.update_main_menu_by_id(db, menu_id=menu_id)
+    return response.status_code
+
+
+# 메뉴 삭제
+@router.delete("/{menu_id}")
+def delete_menu_by_id(menu_id: int, db: Session = Depends(get_db)):
+    response = crud.delete_menu_by_id(db, menu_id = menu_id)
+    return response.status_code
---
+++ backend/api/endpoints/orders.py
@@ -0,0 +1,54 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from crud import crud
+from schemas import schemas
+from api.dep import get_db
+
+router = APIRouter()
+
+
+# TODO: 에러 처리
+
+# 주문
+@router.post("", response_model = schemas.OrderCreate)
+def create_order_info(order: schemas.OrderCreate, db: Session = Depends(get_db)):
+    return crud.create_order(db, order = order)
+
+
+# 주문 조회
+@router.get("", response_model=List[schemas.Order])
+def read_order_info(db: Session = Depends(get_db)):
+    orders = crud.get_order(db)
+    return orders
+
+
+# 주문 상세 조회
+@router.get("/{order_id}")
+def read_order_by_id(order_id: int, db: Session = Depends(get_db)):
+    order = crud.get_order_by_id(db, order_id=order_id)
+    return order
+
+
+# 유저 주문 조회
+@router.get("/user/{user_id}")
+def read_order_by_user_id(user_id: int, db: Session = Depends(get_db)):
+    orders = crud.get_order_by_user_id(db, user_id = user_id)
+    return orders
+
+
+# 가게 주문 조회
+@router.get("/store/{store_id}")
+def read_order_by_store_id(store_id: int, db: Session = Depends(get_db)):
+    orders = crud.get_order_by_store_id(db, store_id = store_id)
+    return orders 
+
+
+# 주문 삭제
+@router.delete("/{order_id}")
+def delete_order_by_id(order_id: int, db: Session = Depends(get_db)):
+    response = crud.delete_order_by_id(db, order_id=order_id)
+    return response.status_code
+
