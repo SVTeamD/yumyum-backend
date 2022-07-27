e9915678e8bb60167a5ba58f0ac290798c11592d
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-07-27 16:22:04 +0900

    refactor: endpoint
    주석 처리 및 crud 함수 적용

--- backend/api/endpoints/users.py
+++ backend/api/endpoints/users.py
@@ -3,7 +3,7 @@ from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from crud import crud
+from crud import user_crud
 from schemas import schemas
 from api.dep import get_db
 
@@ -15,18 +15,18 @@ router = APIRouter()
 # 유저 생성
 @router.post("", response_model=schemas.User)
 def create_user_info(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user=user)
+    return user_crud.create_user(db, user=user)
 
 
 # 유저 상세 조회
 @router.get("/{user_id}")
 def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    users = crud.get_user_by_id(db, user_id=user_id)
+    users = user_crud.get_user_by_id(db, user_id=user_id)
     return users
 
 
 # 유저 삭제
 @router.delete("/{user_id}")
 def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    response = crud.delete_user_by_id(db, user_id=user_id)
+    response = user_crud.delete_user_by_id(db, user_id=user_id)
     return response.status_code
